74ccf9f79ce3539469273d48d3445805bae16345
unknown <akfaqih3@gmail.com>
2024-05-31 09:41:08 -0700

    add forms features

--- debt_management/urls.py
+++ debt_management/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.index,name='Home'),
-    path('transactions/<str:id>',views.transactions,name='transactions')
+    path('transactions/<str:id>',views.transactions,name='transactions'),
+    path('accounts/<str:id>',views.accounts,name='accounts'),
+    path('addTransaction/',views.addTransaction,name='addTransaction'),
+    path('addAccount/',views.addAccount,name='addAccount')
 ]
--- debt_management/views.py
+++ debt_management/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect,HttpResponse
+from django.urls import reverse
 from .models import Account,Transaction
+from datetime import date
 # Create your views here.
 
 def index(request):
@@ -13,5 +15,35 @@ def transactions(request,id):
     filtered =Transaction.objects.filter( account=id)
     withdraw =sum(item.balance for item in filtered.filter(type='سحب') )
     deposit =sum(item.balance for item in filtered.filter(type='إصال') )
-    context = {'Account':Account.objects.get(id=id),'withdraw':withdraw , 'deposit':deposit,'remaining':int(withdraw)-int(deposit), 'transactions':Transaction.objects.filter(account=id)}
+    context = {'Account':Account.objects.get(id=id),'withdraw':withdraw , 'deposit':deposit,'remaining':int(withdraw)-int(deposit), 'transactions':Transaction.objects.filter(account=id),'Accounts':Account.objects.all()}
+    
     return render(request,'transactions.html',context)
+
+def accounts(request,id):
+    filtered = Account.objects.get(pk=id)
+    context={'account':filtered}
+
+    return render(request,'accounts.html',context)
+
+def addTransaction(request):
+    newTransacion= Transaction()
+    newTransacion.type = request.POST.get('type') 
+    newTransacion.account = Account.objects.get(name=request.POST.get('account'))
+    newTransacion.balance = request.POST.get('balance') 
+    newTransacion.content = request.POST.get('content')
+    newTransacion.date = date.today()
+    Transaction.save(newTransacion)
+
+    return HttpResponseRedirect(reverse('transactions',args=str(newTransacion.account.pk)))
+
+def addAccount(request):
+    newAccount=Account()
+    newAccount.name= request.POST.get('name')
+    newAccount.phone= request.POST.get('phone')
+    newAccount.allow_max= request.POST.get('allow_max')
+    newAccount.type = request.POST.get('type')
+    newAccount.active = request.POST.get('active')=='on'
+    newAccount.register_date = date.today()
+    Account.save(newAccount)
+
+    return HttpResponseRedirect(reverse('accounts',args=str(newAccount.pk)))
